19e6fb53d860257b9b98762a9e699b6919563eed
riveur <riveur.cpy@gmail.com>
2024-05-19 19:09:16 +0200

    chore: initial commit

---
+++ inertia/pages/home.tsx
@@ -0,0 +1,13 @@
+import { Head } from '@inertiajs/react'
+
+export default function Home() {
+  return (
+    <>
+      <Head title="Accueil" />
+
+      <div className="container">
+        <div className="title">PalaGuideBot</div>
+      </div>
+    </>
+  )
+}
