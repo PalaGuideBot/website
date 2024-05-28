65c36cdaa9cfbb5abed4491413678a7bd49a9d1e
riveur <riveur.cpy@gmail.com>
2024-05-28 18:45:18 +0200

    refactor(stats/users): extract user_details component

---
+++ inertia/pages/stats/users/components/user_details.tsx
@@ -0,0 +1,500 @@
+import strings from '@adonisjs/core/helpers/string'
+import { ToggleGroup } from '@lemonsqueezy/wedges'
+import { useState } from 'react'
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts'
+import { ArrowRightIcon } from '~/components/icons'
+import PaladiumJob from '~/components/shared/paladium_job'
+import PaladiumRank from '~/components/shared/paladium_rank'
+import ReactSkinview3d from '~/components/skin_viewer_3d'
+import { Card, CardContent, CardHeader, CardTitle } from '~/components/ui/card'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '~/components/ui/table'
+import { smallIcons as smallJobIcons } from '~/content/jobs'
+import { icons as leaderboardIcons } from '~/content/leaderboards'
+import { formatDate } from '~/lib/date'
+import { formatDuration, formatNumber, formatPrice } from '~/lib/utils'
+import type { Job } from '~/types'
+import type { UserShowProps } from '../show'
+
+type UserDetailsProps = {
+  user: NonNullable<UserShowProps['user']>
+}
+
+export const UserDetails = ({ user }: UserDetailsProps) => {
+  const [graphType, setGraphType] = useState<'level' | 'xp'>('level')
+  const sortedUserData = (user?.data || []).sort(
+    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+  )
+
+  const factionHistory = sortedUserData.reduce(
+    (history: { period: string; faction: string }[], data) => {
+      const { faction, date } = {
+        faction: data.data.faction || 'Wilderness',
+        date: data.date,
+      }
+
+      const lastHistory = history[history.length - 1]
+
+      if (!lastHistory || lastHistory.faction !== faction) {
+        history.push({
+          period: `${formatDate(date, 'PP')} au ${formatDate(date, 'PP')}`,
+          faction: faction,
+        })
+      } else {
+        lastHistory.period = `${formatDate(date, 'PP')} au ${lastHistory.period.split(' au ')[1]}`
+      }
+
+      return history
+    },
+    []
+  )
+
+  const rankHistory = sortedUserData.reduce((history: { period: string; rank: string }[], data) => {
+    const { rank, date } = {
+      rank: data.data.rank,
+      date: data.date,
+    }
+
+    const lastHistory = history[history.length - 1]
+
+    if (!lastHistory || lastHistory.rank !== rank) {
+      history.push({
+        period: `${formatDate(date, 'PP')} au ${formatDate(date, 'PP')}`,
+        rank: rank === 'default' ? 'Joueur' : rank,
+      })
+    } else {
+      lastHistory.period = `${formatDate(date, 'PP')} au ${lastHistory.period.split(' au ')[1]}`
+    }
+
+    return history
+  }, [])
+
+  const timePlayedHistory = sortedUserData
+    .toReversed()
+    .reduce((history: { date: string; timePlayed: number; difference: number }[], data) => {
+      const { date } = {
+        date: data.date,
+      }
+
+      const lastHistory = history[history.length - 1]
+
+      if (!lastHistory) {
+        history.push({
+          date: date,
+          timePlayed: data.data.timePlayed,
+          difference: 0,
+        })
+      } else {
+        history.push({
+          date: date,
+          timePlayed: data.data.timePlayed,
+          difference: data.data.timePlayed - lastHistory.timePlayed,
+        })
+      }
+
+      return history
+    }, [])
+
+  const lastUserData = sortedUserData.at(0)
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="grid lg:grid-cols-3 lg:grid-rows-2 gap-4">
+        <Card className="flex flex-col lg:row-span-2">
+          <CardHeader className="border-b">
+            <CardTitle className="text-center font-pixel">{user.username}</CardTitle>
+          </CardHeader>
+          <CardContent className="pt-4 flex-1 flex justify-center">
+            <ReactSkinview3d
+              className="!h-auto max-w-full"
+              width="278"
+              height="450"
+              skinUrl={`https://mineskin.eu/skin/${user.username}`}
+            />
+          </CardContent>
+        </Card>
+        <Card className="flex flex-col lg:col-span-2">
+          <CardHeader className="border-b">
+            <CardTitle>Informations</CardTitle>
+          </CardHeader>
+          <CardContent className="flex-1 pt-4">
+            <ul className="h-full flex flex-col gap-2 justify-around">
+              <li>
+                <InformationLine
+                  label="Première connexion"
+                  value={formatDate(new Date(user.firstJoin), 'PP')}
+                />
+              </li>
+              <li>
+                <InformationLine
+                  label="Rank"
+                  value={
+                    <PaladiumRank
+                      rank={
+                        lastUserData!.data.rank === 'default' ? 'Joueur' : lastUserData!.data.rank
+                      }
+                    />
+                  }
+                />
+              </li>
+              <li>
+                <InformationLine
+                  label="Faction"
+                  value={lastUserData!.data.faction || 'Wilderness'}
+                />
+              </li>
+              <li>
+                <InformationLine label="Money" value={formatPrice(lastUserData!.data.money)} />
+              </li>
+              <li>
+                <InformationLine
+                  label="Temps de jeu"
+                  value={formatDuration(lastUserData!.data.timePlayed)}
+                />
+              </li>
+            </ul>
+          </CardContent>
+        </Card>
+        <Card className="flex lg:col-span-2">
+          <CardContent className="grid grid-cols-2 sm:grid-cols-4 gap-4 items-center flex-1 pt-4">
+            {Object.entries(lastUserData!.data.jobs)
+              .reverse()
+              .map(([job, info]) => (
+                <PaladiumJob key={job} job={job} info={info} />
+              ))}
+          </CardContent>
+        </Card>
+      </div>
+      <Card>
+        <CardHeader className="border-b flex flex-row items-center justify-between py-2">
+          <CardTitle>&Eacute;volution des métiers</CardTitle>
+          <ToggleGroup
+            type="single"
+            value={graphType}
+            onValueChange={(value) => {
+              if (value.length) {
+                setGraphType(value as 'level' | 'xp')
+              }
+            }}
+            size="sm"
+            className="!m-0"
+          >
+            <ToggleGroup.Item value="level">Level</ToggleGroup.Item>
+            <ToggleGroup.Item value="xp">XP</ToggleGroup.Item>
+          </ToggleGroup>
+        </CardHeader>
+        <CardContent className="p-0 h-64">
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart height={200} data={sortedUserData.toReversed()}>
+              <CartesianGrid strokeDasharray="3 3" horizontal={false} />
+              <XAxis dataKey="date" className="text-xs" />
+              <YAxis
+                domain={graphType === 'level' ? [0, 100] : undefined}
+                orientation="right"
+                className="text-xs"
+                tickFormatter={(value) => formatNumber(Number(value))}
+              />
+              <Tooltip
+                content={({ active, payload, label }) => {
+                  if (active && payload && payload.length) {
+                    return (
+                      <Card className="bg-background">
+                        <CardContent className="p-4 space-y-2">
+                          <div className="font-pixel text-xs">{formatDate(label, 'PP')}</div>
+                          <div className="flex flex-col gap-2">
+                            {payload.map(({ name, value }) => {
+                              const Icon = smallJobIcons[name as Job]
+                              return (
+                                <div key={name} className="flex gap-2 items-center">
+                                  <span className="text-sm">
+                                    <Icon className="w-4 mr-2" />
+                                    {name &&
+                                      name.toString().charAt(0).toUpperCase() +
+                                        name.toString().slice(1)}
+                                  </span>
+                                  <span className="text-sm font-bold">
+                                    {formatNumber(Number(value), {
+                                      notation: 'standard',
+                                      maximumFractionDigits: 2,
+                                    })}
+                                  </span>
+                                </div>
+                              )
+                            })}
+                          </div>
+                        </CardContent>
+                      </Card>
+                    )
+                  }
+                  return null
+                }}
+              />
+              <Legend
+                formatter={(value) =>
+                  value.toString().charAt(0).toUpperCase() + value.toString().slice(1)
+                }
+              />
+              {Object.keys(user.data[0].data.jobs)
+                .reverse()
+                .map((job) => (
+                  <Line
+                    key={job}
+                    type="monotone"
+                    dataKey={`data.jobs.${job}.${graphType}`}
+                    name={job}
+                    stroke={`var(--job-${job})`}
+                  />
+                ))}
+            </LineChart>
+          </ResponsiveContainer>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader className="border-b">
+          <CardTitle>&Eacute;volution du temps de jeu</CardTitle>
+        </CardHeader>
+        <CardContent className="p-0 h-64">
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart height={200} data={timePlayedHistory}>
+              <CartesianGrid strokeDasharray="3 3" horizontal={false} />
+              <XAxis dataKey="date" className="text-xs" />
+              <YAxis
+                className="text-xs"
+                tickFormatter={(value) => formatNumber(Number(value))}
+                orientation="right"
+              />
+              <Tooltip
+                content={({ active, payload, label }) => {
+                  if (active && payload && payload.length) {
+                    const { difference, timePlayed } = payload[0].payload
+                    return (
+                      <Card className="bg-background">
+                        <CardContent className="p-4 space-y-2">
+                          <div className="font-pixel text-xs">{formatDate(label, 'PP')}</div>
+                          <div className="flex flex-col gap-2">
+                            <div className="flex gap-2 items-center">
+                              <span className="text-sm">Temps de jeu: </span>
+                              <span className="text-sm font-bold">
+                                {formatDuration(timePlayed)}
+                              </span>
+                            </div>
+                            <div className="flex gap-2 items-center">
+                              <span className="text-sm">Différence: </span>
+                              <span className="text-sm font-bold">
+                                {formatNumber(difference, {
+                                  notation: 'standard',
+                                  maximumFractionDigits: 2,
+                                })}{' '}
+                                minutes
+                              </span>
+                            </div>
+                          </div>
+                        </CardContent>
+                      </Card>
+                    )
+                  }
+                  return null
+                }}
+              />
+              <Legend />
+              <Line
+                type="monotone"
+                dataKey="difference"
+                name="Différence de temps de jeu"
+                stroke="#8884d8"
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader className="border-b">
+          <CardTitle>&Eacute;volution de l'argent</CardTitle>
+        </CardHeader>
+        <CardContent className="p-0 h-64">
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart height={200} data={sortedUserData.toReversed()}>
+              <CartesianGrid strokeDasharray="3 3" horizontal={false} />
+              <XAxis dataKey="date" className="text-xs" />
+              <YAxis
+                className="text-xs"
+                tickFormatter={(value) => formatPrice(Number(value))}
+                orientation="right"
+              />
+              <Tooltip
+                content={({ active, payload, label }) => {
+                  if (active && payload && payload.length) {
+                    return (
+                      <Card className="bg-background">
+                        <CardContent className="p-4 space-y-2">
+                          <div className="font-pixel text-xs">{formatDate(label, 'PP')}</div>
+                          <div className="flex gap-2 items-center">
+                            <span className="text-sm">Argent: </span>
+                            <span className="text-sm font-bold">
+                              {formatPrice(Number(payload[0].value))}
+                            </span>
+                          </div>
+                        </CardContent>
+                      </Card>
+                    )
+                  }
+                  return null
+                }}
+              />
+              <Legend />
+              <Line
+                type="monotone"
+                dataKey="data.money"
+                name="Argent"
+                stroke="#82ca9d"
+                dot={false}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader className="border-b">
+          <CardTitle>Classements</CardTitle>
+        </CardHeader>
+        <CardContent className="pt-4 grid grid-cols-1 sm:grid-cols-2 gap-4">
+          {Object.entries(user.leaderboard)
+            .filter(([key]) => !['corruption'].includes(key))
+            .map(([key, value]) => {
+              const Icon = leaderboardIcons[key as keyof typeof leaderboardIcons]
+              return (
+                <div
+                  key={key}
+                  className="flex gap-4 border p-4 bg-background/50 rounded-md hover:bg-background/30"
+                >
+                  {Icon && <Icon className="w-12 invert-0" />}
+                  <div className="flex flex-col gap-2">
+                    <span className="font-pixel text-xs">{strings.noCase(key)}</span>
+                    <span className="text-sm text-primary font-mc-dungueons">
+                      # {formatNumber(value, { notation: 'standard' })}
+                    </span>
+                  </div>
+                </div>
+              )
+            })}
+        </CardContent>
+      </Card>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <Card>
+          <CardHeader className="border-b">
+            <CardTitle>Historique des factions</CardTitle>
+          </CardHeader>
+          <CardContent className="p-0">
+            <Table className="text-nowrap">
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Période</TableHead>
+                  <TableHead>Faction</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {factionHistory.map((entry, index) => (
+                  <TableRow key={index}>
+                    <TableCell>{entry.period}</TableCell>
+                    <TableCell>{entry.faction}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="border-b">
+            <CardTitle>Historique des rangs</CardTitle>
+          </CardHeader>
+          <CardContent className="p-0">
+            <Table className="text-nowrap">
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Période</TableHead>
+                  <TableHead>Rank</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {rankHistory.map((entry, index) => (
+                  <TableRow key={index}>
+                    <TableCell>{entry.period}</TableCell>
+                    <TableCell>
+                      <PaladiumRank rank={entry.rank} className="text-xs" />
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </CardContent>
+        </Card>
+      </div>
+      <Card>
+        <CardHeader className="border-b">
+          <CardTitle>Historique des données</CardTitle>
+        </CardHeader>
+        <CardContent className="p-0">
+          <Table className="text-nowrap">
+            <TableHeader>
+              <TableRow>
+                <TableHead>Date</TableHead>
+                <TableHead>Rank</TableHead>
+                <TableHead>Faction</TableHead>
+                <TableHead>Money</TableHead>
+                <TableHead>Temps de jeu</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {user.data.map((data) => (
+                <TableRow key={data.date}>
+                  <TableCell>{formatDate(data.date, 'PP')}</TableCell>
+                  <TableCell>
+                    <PaladiumRank rank={data.data.rank} className="text-xs" />
+                  </TableCell>
+                  <TableCell>{data.data.faction || 'Wilderness'}</TableCell>
+                  <TableCell>{formatPrice(data.data.money)}</TableCell>
+                  <TableCell>{formatDuration(data.data.timePlayed)}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
+
+const InformationLine = ({
+  label,
+  value,
+}: {
+  label: string
+  value: string | number | React.ReactNode
+}) => {
+  return (
+    <div className="flex gap-2">
+      <span className="font-pixel">{label}</span>
+      <ArrowRightIcon className="w-2" />
+      {typeof value === 'string' || typeof value === 'number' ? (
+        <span className="text-sm font-mc-dungueons">{value}</span>
+      ) : (
+        value
+      )}
+    </div>
+  )
+}
