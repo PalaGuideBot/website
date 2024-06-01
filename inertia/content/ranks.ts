edb4374028819943d4db25b832ed88d2bc748a44
Zeluck-Github <nonocreatif@gmail.com>
2024-06-01 23:12:07 +0200

    Add new rank icons

--- inertia/content/ranks.ts
+++ inertia/content/ranks.ts
@@ -3,6 +3,15 @@ import paladinIcon from '~/assets/rank-icons/paladin.png'
 import titanIcon from '~/assets/rank-icons/titan.png'
 import trixiumPlusIcon from '~/assets/rank-icons/trixium-plus.png'
 import trixiumIcon from '~/assets/rank-icons/trixium.png'
+import youtuberIcon from '~/assets/rank-icons/youtuber.png'
+import streamerIcon from '~/assets/rank-icons/streamer.png'
+import ingameIcon from '~/assets/rank-icons/ingame.png'
+import palastaffIcon from '~/assets/rank-icons/palastaff.png'
+import discordIcon from '~/assets/rank-icons/discord.png'
+import graphisteIcon from '~/assets/rank-icons/graphiste.png'
+import redacteurIcon from '~/assets/rank-icons/redaction.png'
+import gamedesignIcon from '~/assets/rank-icons/gamedesign.png'
+import developpeurIcon from '~/assets/rank-icons/developpeur.png'
 import type { Rank } from '~/types'
 
 export const icons: Record<Rank, string> = {
@@ -11,4 +20,22 @@ export const icons: Record<Rank, string> = {
   'endium': endiumIcon,
   'trixium': trixiumIcon,
   'trixium+': trixiumPlusIcon,
+  'youtuber': youtuberIcon,
+  'streamer': streamerIcon,
+  'moderateurchat': ingameIcon,
+  'moderateur': ingameIcon,
+  'moderateursenior': ingameIcon,
+  'operateur': ingameIcon,
+  'operateurcertif': ingameIcon,
+  'operateursenior': ingameIcon,
+  'paladium-staff': palastaffIcon,
+  'discord': discordIcon,
+  'discord+': discordIcon,
+  'graphistejunior': graphisteIcon,
+  'graphiste': graphisteIcon,
+  'création': graphisteIcon,
+  'redacteur': redacteurIcon,
+  'gamedesigner': gamedesignIcon,
+  'developpeur': developpeurIcon,
+  'developpeursenior': developpeurIcon,
 }
