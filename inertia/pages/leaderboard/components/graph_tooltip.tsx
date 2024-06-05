d3788b85ab719796065f9df620bdeb4ff43e823b
riveur <riveur.cpy@gmail.com>
2024-06-05 18:26:35 +0200

    refactor: sort leaderboard graph toolitp data

--- inertia/pages/leaderboard/components/graph_tooltip.tsx
+++ inertia/pages/leaderboard/components/graph_tooltip.tsx
@@ -18,17 +18,19 @@ export const GraphTooltip = <TValue extends ValueType, TName extends NameType>({
       <Card className="bg-background">
         <CardContent className="p-4 space-y-2 min-w-52">
           <div className="font-pixel text-xs">{formatDate(label, 'PP')}</div>
-          {payload.map((p, index) => (
-            <div key={p.dataKey} className="flex gap-2 items-center text-sm">
-              {pageOffset !== undefined && (
-                <span className="font-mc-dungueons text-xs"># {index + 1 + pageOffset}</span>
-              )}
-              <span>{p.dataKey}</span>
-              <span className="font-bold">
-                {valueFormatter ? valueFormatter(p.value) : p.value}
-              </span>
-            </div>
-          ))}
+          {payload
+            .toSorted((a, b) => Number(b.value) - Number(a.value))
+            .map((p, index) => (
+              <div key={p.dataKey} className="flex gap-2 items-center text-sm">
+                {pageOffset !== undefined && (
+                  <span className="font-mc-dungueons text-xs"># {index + 1 + pageOffset}</span>
+                )}
+                <span>{p.dataKey}</span>
+                <span className="font-bold">
+                  {valueFormatter ? valueFormatter(p.value) : p.value}
+                </span>
+              </div>
+            ))}
         </CardContent>
       </Card>
     )
