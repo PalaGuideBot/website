9e08fa962454170f34edac72e5bea34e95135941
riveur <riveur.cpy@gmail.com>
2024-06-20 19:21:50 +0200

    refactor: improve link trigger

--- inertia/components/shared/link_trigger.tsx
+++ inertia/components/shared/link_trigger.tsx
@@ -10,8 +10,14 @@ const LinkTrigger = ({
   return (
     <div className="group flex items-center gap-2">
       {children}
-      <Button className="p-0.5" variant="transparent" asChild>
-        <a {...props} className={cn('inline-block opacity-0 group-hover:opacity-100', className)}>
+      <Button className="p-0.5" variant="tertiary" asChild>
+        <a
+          {...props}
+          className={cn(
+            'inline-block opacity-0 group-hover:opacity-100 transition-opacity',
+            className
+          )}
+        >
           <HashIcon className="size-4" />
         </a>
       </Button>
