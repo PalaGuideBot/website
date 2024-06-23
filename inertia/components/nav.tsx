85b0e83b27b80ec354809f0d9bb693c09424fb98
riveur <riveur.cpy@gmail.com>
2024-06-23 16:59:06 +0200

    refactor: revert sidebar navigation

--- inertia/components/nav.tsx
+++ inertia/components/nav.tsx
@@ -3,7 +3,6 @@ import { Button } from '@lemonsqueezy/wedges'
 import {
   BadgeEuroIcon,
   BotIcon,
-  ChevronsRightIcon,
   DiamondIcon,
   EggIcon,
   ExternalLinkIcon,
@@ -34,125 +33,108 @@ function createIconLink(Icon: LucideIcon) {
   return <Icon className="h-4 w-4" />
 }
 
-const LINKS: Array<{ path: string; label: string; items: Array<LinkProps> }> = [
-  {
-    path: '/stats',
-    label: 'Statistiques',
-    items: [
-      {
-        path: '/stats/users',
-        label: 'Utilisateur',
-        icon: createIconLink(UserIcon),
-      },
-      {
-        path: '/stats/factions',
-        label: 'Faction',
-        icon: createIconLink(ShieldIcon),
-      },
-    ],
-  },
-  {
-    path: '/leaderboard',
-    label: 'Classements',
-    items: [
-      {
-        path: '/leaderboard/factions',
-        label: 'Factions',
-        icon: createIconLink(UsersIcon),
-      },
-      {
-        path: '/leaderboard/money',
-        label: 'Money',
-        icon: createIconLink(BadgeEuroIcon),
-      },
-      {
-        path: '/leaderboard/trixium',
-        label: 'Trixium',
-        icon: createIconLink(DiamondIcon),
-      },
-      {
-        path: '/leaderboard/clicker',
-        label: 'Clicker',
-        icon: createIconLink(HandCoinsIcon),
-      },
-      {
-        path: '/leaderboard/boss',
-        label: 'Boss',
-        icon: createIconLink(SkullIcon),
-      },
-      /* {
+const LINKS: Record<string, Array<LinkProps>> = {
+  Statistiques: [
+    {
+      path: '/stats/users',
+      label: 'Utilisateur',
+      icon: createIconLink(UserIcon),
+    },
+    {
+      path: '/stats/factions',
+      label: 'Faction',
+      icon: createIconLink(ShieldIcon),
+    },
+  ],
+  Classement: [
+    {
+      path: '/leaderboard/factions',
+      label: 'Factions',
+      icon: createIconLink(UsersIcon),
+    },
+    {
+      path: '/leaderboard/money',
+      label: 'Money',
+      icon: createIconLink(BadgeEuroIcon),
+    },
+    {
+      path: '/leaderboard/trixium',
+      label: 'Trixium',
+      icon: createIconLink(DiamondIcon),
+    },
+    {
+      path: '/leaderboard/clicker',
+      label: 'Clicker',
+      icon: createIconLink(HandCoinsIcon),
+    },
+    {
+      path: '/leaderboard/boss',
+      label: 'Boss',
+      icon: createIconLink(SkullIcon),
+    },
+    /* {
       path: '/leaderboard/chorus',
       label: 'Chorus',
       icon: createIconLink(DiamondIcon),
     }, */
-      {
-        path: '/leaderboard/egghunt',
-        label: 'EggHunt',
-        icon: createIconLink(EggIcon),
-      },
-      /* {
+    {
+      path: '/leaderboard/egghunt',
+      label: 'EggHunt',
+      icon: createIconLink(EggIcon),
+    },
+    /* {
       path: '/leaderboard/end',
       label: 'End',
       icon: createIconLink(DiamondIcon),
     }, */
-      {
-        path: '/leaderboard/koth',
-        label: 'Koth',
-        icon: createIconLink(SwordsIcon),
-      },
-    ],
-  },
-  {
-    path: '/status',
-    label: 'Status',
-    items: [
-      {
-        path: 'https://status.palaguidebot.fr',
-        label: 'Bot',
-        icon: createIconLink(BotIcon),
-        external: true,
-      },
-      {
-        path: '/status/paladium',
-        label: 'Paladium',
-        icon: createIconLink(GamepadIcon),
-      },
-    ],
-  },
-  {
-    path: '/informations',
-    label: 'Informations',
-    items: [
-      {
-        path: '/privacy',
-        label: 'Politique de confidentialité',
-        icon: createIconLink(ShieldQuestionIcon),
-      },
-      {
-        path: '/terms',
-        label: "Conditions d'utilisation",
-        icon: createIconLink(UserCogIcon),
-      },
-      {
-        path: '/changelogs',
-        label: 'Changelogs',
-        icon: createIconLink(FileEditIcon),
-      },
-    ],
-  },
-]
+    {
+      path: '/leaderboard/koth',
+      label: 'Koth',
+      icon: createIconLink(SwordsIcon),
+    },
+  ],
+  Status: [
+    {
+      path: 'https://status.palaguidebot.fr',
+      label: 'Bot',
+      icon: createIconLink(BotIcon),
+      external: true,
+    },
+    {
+      path: '/status/paladium',
+      label: 'Paladium',
+      icon: createIconLink(GamepadIcon),
+    },
+  ],
+  Informations: [
+    {
+      path: '/privacy',
+      label: 'Politique de confidentialité',
+      icon: createIconLink(ShieldQuestionIcon),
+    },
+    {
+      path: '/terms',
+      label: "Conditions d'utilisation",
+      icon: createIconLink(UserCogIcon),
+    },
+    {
+      path: '/changelogs',
+      label: 'Changelogs',
+      icon: createIconLink(FileEditIcon),
+    },
+  ],
+}
 
 export function Navigation() {
   const { url } = usePage()
   return (
     <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-      {LINKS.map(({ items: links, ...category }) => (
-        <div key={category.label}>
-          <Link href={category.path} className="block py-4 tracking-wider hover:underline">
-            <ChevronsRightIcon className="size-4 inline-block mr-2" />
-            <span>{category.label}</span>
-          </Link>
-          <ul className="flex flex-col gap-1 [&>li]:ml-2">
+      {Object.entries(LINKS).map(([category, links]) => (
+        <div key={category}>
+          <h2 className="text-xs font-semibold text-foreground/50 uppercase tracking-wider my-2">
+            {category}
+          </h2>
+          <ul className="flex flex-col gap-1 mb-2">
             {links.map(({ path, label, icon, external = false }) => (
               <li key={path}>
                 {external ? (
