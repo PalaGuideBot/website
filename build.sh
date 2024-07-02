8f7b7465acf8aba858b9858e76e37dcb7a675434
Tony Kun <doudeville@outlook.fr>
2024-07-02 19:06:54 +0200

    new build

--- build.sh
+++ build.sh
@@ -1,47 +1,19 @@
 #!/bin/bash
 
-# Fetch all branches from the remote repository
-
+# Fetch git
 git fetch -a
 
-# Pull the latest changes from the remote repository
-
+# Pull git
 git pull
 
-# Vérifie si npm est installé
-if ! command -v npm &> /dev/null
-then
-    echo "npm could not be found, please install it first."
-    exit 1
-fi
+# Install npm
+npm install
 
-# Exécute la commande npm run build
-echo "Running npm run build..."
+# Build
 npm run build
 
-# Vérifie si la commande npm run build a réussi
-if [ $? -ne 0 ]; then
-    echo "npm run build failed, exiting script."
-    exit 1
-fi
-
-# Vérifie si le fichier .env existe
-if [ ! -f .env ]; then
-    echo ".env file does not exist."
-    exit 1
-fi
-
-# Vérifie si le dossier build existe
-if [ ! -d build ]; then
-    echo "build directory does not exist. Creating build directory."
-    mkdir build
-fi
-
-# Copie le fichier .env dans le dossier build
-echo "Copying .env to build directory..."
+# Copy .env
 cp .env build/
 
-# Restart the WEBSITE application using pm2
+# Restart the website
 pm2 restart website
-
-echo "Script completed successfully."
