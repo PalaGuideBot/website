720a5a3f00341e3ac3a117a9235c4d6e21dc6b8f
riveur <riveur.cpy@gmail.com>
2024-08-05 20:23:12 +0200

    refactor(profile): UUID to uuid

--- app/core/validators/minecraft_valiadator.ts
+++ app/core/validators/minecraft_valiadator.ts
@@ -3,7 +3,7 @@ import vine from '@vinejs/vine'
 export const minecraftAccountLinkValidator = vine.compile(
   vine.object({
     message: vine.string(),
-    UUID: vine.string().uuid(),
+    uuid: vine.string().uuid(),
     username: vine.string(),
   })
 )
