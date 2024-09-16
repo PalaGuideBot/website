afa4d68efa4489932b071b722110c19f881cd6e0
riveur <riveur.cpy@gmail.com>
2024-09-16 18:47:05 +0200

    refactor(ui): improve table hover

--- inertia/components/ui/table.tsx
+++ inertia/components/ui/table.tsx
@@ -44,7 +44,7 @@ const TableRow = React.forwardRef<HTMLTableRowElement, React.HTMLAttributes<HTML
     <tr
       ref={ref}
       className={cn(
-        'border-b transition-colors hover:bg-surface/50 data-[state=selected]:bg-surface',
+        'border-b transition-colors hover:bg-black/15 dark:hover:bg-surface/10 data-[state=selected]:bg-surface',
         className
       )}
       {...props}
