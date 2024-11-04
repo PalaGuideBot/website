e2bd0edf1de3e2ad257306c4bc39265a12839b8b
Tony Kun <doudeville@outlook.fr>
2024-11-04 09:46:54 +0100

    fix(faction): redirect to user's faction if authenticated and no faction name provided

--- app/stats/controllers/faction_controller.ts
+++ app/stats/controllers/faction_controller.ts
@@ -9,9 +9,22 @@ import { ApiService } from '#core/services/api'
 export default class FactionController {
   constructor(private api: ApiService) {}
 
-  async show({ inertia, params }: HttpContext) {
+  async show({ inertia, response, params, auth }: HttpContext) {
     let faction = null
     let exampleFaction = null
+
+    try {
+      if (auth?.user && !params.name) {
+        console.log('auth?.user', auth?.user)
+        const profile = await this.api.getMinecraftAccountLinked(auth.user!.id)
+        console.log('profile', profile)
+        const player = await this.api.getPlayer(profile.username)
+        console.log('player', player)
+        console.log('faction', player.data.at(-1)?.data.faction)
+        return response.redirect(`/factions/${player.data.at(-1)?.data.faction}`)
+      }
+    } catch {}
+
     try {
       if (params.name) {
         faction = await this.api.getFaction(params.name)
