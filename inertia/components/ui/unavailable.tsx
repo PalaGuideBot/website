a46eec87deeff8b35f924758d5c0674da707ae95
Zeluck <nonocreatif@gmail.com>
2024-12-21 02:37:54 +0100

    fix: improve error message clarity in Unavailable component

--- inertia/components/ui/unavailable.tsx
+++ inertia/components/ui/unavailable.tsx
@@ -3,8 +3,8 @@ import { Alert } from '@lemonsqueezy/wedges'
 const Unavailable = () => {
   return (
     <Alert color="error" variant="expanded" closable>
-      L'information est actuellement indisponible via l'API de Paladium pour le moment. Si vous avez
-      des questions, n'hésitez pas à nous les poser sur Discord.
+      L'information est actuellement indisponible via l'API de Paladium. Si vous avez des questions,
+      n'hésitez pas à nous les poser sur Discord.
     </Alert>
   )
 }
