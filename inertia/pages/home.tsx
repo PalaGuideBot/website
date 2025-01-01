9f8bb6d7a9496ee8970a0913b82fc0cfba5b3e7f
riveur <riveur.cpy@gmail.com>
2025-01-01 17:20:32 +0100

    refactor: remove noel theme

--- inertia/pages/home.tsx
+++ inertia/pages/home.tsx
@@ -1,15 +1,15 @@
-import type PageController from '#controllers/page_controller'
 import { InferPageProps } from '@adonisjs/inertia/types'
 import { Link } from '@inertiajs/react'
 import { Button } from '@lemonsqueezy/wedges'
 import Autoplay from 'embla-carousel-autoplay'
 import { ArrowRightIcon, ExternalLinkIcon } from 'lucide-react'
+
+import type PageController from '#controllers/page_controller'
 import { DiscordIcon } from '~/components/icons'
 import DefaultLayout from '~/components/layouts/default'
 import { CreditCard } from '~/components/shared/credit_card'
 import { GiveawayBanner } from '~/components/shared/giveaway_banner'
 import { Head } from '~/components/shared/head'
-import { Snowfall } from '~/components/shared/snowfall'
 import { Card, CardContent } from '~/components/ui/card'
 import { Carousel, CarouselContent, CarouselItem } from '~/components/ui/carousel'
 import { formatNumber } from '~/lib/utils'
@@ -26,7 +26,6 @@ export default function Home(props: HomePageProps) {
       <Head descriptors={[{ title: 'Accueil' }]} />
       <DefaultLayout className="relative p-0 lg:p-0 gap-0 lg:gap-0">
         {/* {isActiveGiveaway && <GiveawayBanner className="rounded-none min-h-[60px]" />} */}
-        <Snowfall />
         <section className="p-4 lg:p-6 max-w-7xl mx-auto min-h-dvh grid items-center border-b">
           <div className="grid lg:grid-cols-2 gap-8 items-center justify-center">
             <div className="space-y-6">
