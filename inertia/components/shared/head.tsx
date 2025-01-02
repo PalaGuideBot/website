42a1ab9b6c58be46575d6502989f198060a4972e
Zeluck <nonocreatif@gmail.com>
2025-01-02 13:20:38 +0100

    fix(head): correct grammatical error in page description

--- inertia/components/shared/head.tsx
+++ inertia/components/shared/head.tsx
@@ -37,7 +37,7 @@ const Head = ({
   descriptors = [],
 }: React.ComponentProps<typeof InertiaHead> & { descriptors?: MetaDescriptor[] }) => {
   const description =
-    'PalaGuideBot vous met à disposition des outils qui vous accompagne dans votre aventure sur Paladium.'
+    'PalaGuideBot vous met à disposition des outils qui vous accompagnent dans votre aventure sur Paladium.'
 
   const titleMeta = descriptors.find((d) => 'title' in d) as { title: string } | undefined
   const title = titleMeta ? `${titleMeta.title} - PalaGuideBot` : 'PalaGuideBot'
