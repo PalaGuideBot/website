2ccda2d5aa6bc0c010999e3223c84d292959a3ae
riveur <riveur.cpy@gmail.com>
2025-02-13 05:39:48 +0100

    refactor: make pet & mount more customizable

--- inertia/components/three/pet.tsx
+++ inertia/components/three/pet.tsx
@@ -6,7 +6,13 @@ import * as THREE from 'three'
 import { PetType } from '~/content/pets'
 import { cn } from '~/lib/utils'
 
-const PetModel = ({ model, isLooping }: { model: PetType; isLooping: boolean }) => {
+interface PetProps {
+  model: PetType
+  isLooping: boolean
+  rotation?: [number, number, number]
+}
+
+const PetModel = ({ model, isLooping, rotation = [0, Math.PI / 0.8, 0] }: PetProps) => {
   const { scene, animations } = useGLTF(`/pet-model/${model}.gltf`)
   const { actions } = useAnimations(animations, scene)
 
@@ -28,29 +34,31 @@ const PetModel = ({ model, isLooping }: { model: PetType; isLooping: boolean }) 
     }
   }, [isLooping, actions])
 
-  return (
-    <primitive object={scene} scale={3.0} rotation={[0, Math.PI / 0.8, 0]} position={[0, -1, 0]} />
-  )
+  return <primitive object={scene} scale={3.0} rotation={rotation} position={[0, -1, 0]} />
 }
 
 interface PetViewerProps extends Omit<React.ComponentProps<typeof Canvas>, 'children'> {
   model: PetType
   isLooping: boolean
+  enableControls?: boolean
+  rotation?: PetProps['rotation']
 }
 
 export const PetViewer = React.forwardRef<HTMLCanvasElement, PetViewerProps>(
-  ({ model, className, isLooping, ...props }, ref) => (
+  ({ model, className, enableControls = true, rotation, isLooping, ...props }, ref) => (
     <Canvas ref={ref} className={cn('!h-[200px]', className)} {...props}>
       <ambientLight intensity={1} />
       <directionalLight position={[10, 10, 5]} intensity={1} />
-      <PetModel model={model} isLooping={isLooping} />
-      <OrbitControls
-        enableZoom={false}
-        enableRotate={true}
-        target={[0, 0.5, 0]}
-        enablePan={false}
-        rotateSpeed={0.3}
-      />
+      <PetModel model={model} isLooping={isLooping} rotation={rotation} />
+      {enableControls && (
+        <OrbitControls
+          enableZoom={false}
+          enableRotate={true}
+          target={[0, 0.5, 0]}
+          enablePan={false}
+          rotateSpeed={0.3}
+        />
+      )}
     </Canvas>
   )
 )
