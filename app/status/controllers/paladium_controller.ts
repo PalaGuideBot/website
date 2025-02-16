42d68ef499f20ef0eb484fc61b49a4bc4e863bf3
riveur <riveur.cpy@gmail.com>
2025-02-16 15:44:48 +0100

    refactor: re-enable status/paladium page

--- app/status/controllers/paladium_controller.ts
+++ app/status/controllers/paladium_controller.ts
@@ -1,13 +1,14 @@
-// import { ApiService } from '#core/services/api'
-// import { inject } from '@adonisjs/core'
+import { inject } from '@adonisjs/core'
 import { HttpContext } from '@adonisjs/core/http'
 
-// @inject()
+import { ApiService } from '#core/services/api'
+
+@inject()
 export default class PaladiumController {
-  // constructor(private api: ApiService) {}
+  constructor(private api: ApiService) {}
 
   async index({ inertia }: HttpContext) {
-    // const { hour: todayStatus, day: last30daysStatus } = await this.api.getPaladiumStatus()
-    return inertia.render('status/paladium/index' /* { todayStatus, last30daysStatus } */)
+    const { hour: todayStatus, day: last30daysStatus } = await this.api.getPaladiumStatus()
+    return inertia.render('status/paladium/index', { todayStatus, last30daysStatus })
   }
 }
