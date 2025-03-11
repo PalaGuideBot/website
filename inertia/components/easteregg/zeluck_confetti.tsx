f270839753a72fcf3c3b0420542e1fee8a279d9f
Zeluck <nonocreatif@gmail.com>
2025-03-11 18:37:32 +0100

    feat(confetti): replace tkt faut pas regarder

--- inertia/components/easteregg/zeluck_confetti.tsx
+++ inertia/components/easteregg/zeluck_confetti.tsx
@@ -1,29 +1,45 @@
-import { ConfettiButton, safeShapeFromText } from '~/components/magicui/confetti'
+import confetti from 'canvas-confetti'
+import { Button } from '@lemonsqueezy/wedges'
 
-const ZeluckConfetti = () => {
+export function ZeluckConfetti() {
+  const CanonConfetti = () => {
+    const end = Date.now() + 3 * 1000 // 3 seconds
+    const colors = ['#ffb700', '#2dca72', '#6c6c89', '#f53d6b']
+
+    const frame = () => {
+      if (Date.now() > end) return
+
+      confetti({
+        particleCount: 2,
+        angle: 60,
+        spread: 55,
+        startVelocity: 60,
+        origin: { x: 0, y: 0.5 },
+        colors: colors,
+      })
+      confetti({
+        particleCount: 2,
+        angle: 120,
+        spread: 55,
+        startVelocity: 60,
+        origin: { x: 1, y: 0.5 },
+        colors: colors,
+      })
+
+      requestAnimationFrame(frame)
+    }
+
+    frame()
+  }
+
   return (
-    <ConfettiButton
+    <Button
       variant="link"
       size="sm"
       className="text-xs text-primary no-underline cursor-default [&>span]:p-0"
-      options={{
-        particleCount: 5,
-        spread: 100,
-        ticks: 120,
-        gravity: 0,
-        decay: 0.96,
-        angle: 120,
-        shapes: [
-          safeShapeFromText({ text: '🎨', scalar: 4 }),
-          safeShapeFromText({ text: '✏️', scalar: 4 }),
-          safeShapeFromText({ text: '💻', scalar: 4 }),
-        ],
-        scalar: 5,
-      }}
+      onClick={CanonConfetti}
     >
       Zeluck
-    </ConfettiButton>
+    </Button>
   )
 }
-
-export { ZeluckConfetti }
