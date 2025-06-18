e52b8bac7525fb83593e0e5464062a78ea2f55a0
riveur <riveur.cpy@gmail.com>
2025-06-18 18:59:51 +0200

    refactor: remove text shadow

--- inertia/pages/stats/components/information_line.tsx
+++ inertia/pages/stats/components/information_line.tsx
@@ -9,7 +9,7 @@ export function InformationLine({
 }) {
   return (
     <div className="flex gap-2 items-center">
-      <span className="dark:text-shadow-[0px_1px_2px_black] font-pixel text-pretty">{label}</span>
+      <span className="font-pixel text-pretty">{label}</span>
       <ChevronsRightIcon className="h-4" />
       {typeof value === 'string' || typeof value === 'number' ? (
         <span className="text-xs sm:text-sm font-mc-dungueons">{value}</span>
