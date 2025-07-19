4b66bb4b62ffa8d4f028ef290e9207ead374a723
Zeluck-github <nonocreatif@gmail.com>
2025-07-19 06:28:27 +0200

    fix: comment out unused PaladiumWrappedBanner and wrapped routes

--- inertia/components/app_sidebar.tsx
+++ inertia/components/app_sidebar.tsx
@@ -38,7 +38,7 @@ import {
 import { Tooltip, TooltipContent, TooltipTrigger } from '~/components/ui/tooltip'
 import { useAuth } from '~/hooks/use_auth'
 import { useSidebarStateStore } from '~/stores/sidebar_state_store'
-import { PaladiumWrappedBanner } from './shared/paladium_wrapped_banner'
+/* import { PaladiumWrappedBanner } from './shared/paladium_wrapped_banner' */
 
 type LinkProps = {
   title: string
@@ -294,9 +294,7 @@ export function AppSidebar() {
       </SidebarContent>
       <SidebarFooter>
         <SidebarMenu>
-          <div className="p-1">
-            <PaladiumWrappedBanner />
-          </div>
+          <div className="p-1">{/*<PaladiumWrappedBanner /> */}</div>
           <SidebarMenuItem>
             {user ? (
               <DropdownMenu>
